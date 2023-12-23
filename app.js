41576a9948254141e72493ca9ab01c740c0421ee
Olimjon-04042000 <nishonovolimjon2000@gmail.com>
2023-12-23 07:42:18 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// async function getData() {
+//     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+//     const data = await res.json();
+
+//     console.log(data);
+// }
+// getData();
+// console.log('salom');
